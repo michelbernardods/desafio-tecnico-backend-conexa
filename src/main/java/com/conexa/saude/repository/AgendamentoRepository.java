7cbf20a8ceacb08607b865dc9d6316748576f8ff
Michel Bernardo <michel_13s@outlook.com>
2023-12-28 13:13:46 -0300

    feat: creating model for agendamento and paciente

---
+++ src/main/java/com/conexa/saude/repository/AgendamentoRepository.java
@@ -0,0 +1,9 @@
+package com.conexa.saude.repository;
+
+import com.conexa.saude.model.Agendamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
+}
---
+++ src/main/java/com/conexa/saude/repository/PacienteRepository.java
@@ -0,0 +1,10 @@
+package com.conexa.saude.repository;
+
+import com.conexa.saude.model.Paciente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Paciente findByCpf(String cpf);
+}
