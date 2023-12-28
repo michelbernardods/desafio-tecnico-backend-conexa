5f2ac339129511819c3aebd73833fbcdb4db6b32
Michel Bernardo <michel_13s@outlook.com>
2023-12-28 13:14:50 -0300

    feat: create router for register paciente and refactor code

--- src/main/java/com/conexa/saude/controller/MedicoController.java
+++ src/main/java/com/conexa/saude/controller/MedicoController.java
@@ -1,30 +1,44 @@
 package com.conexa.saude.controller;
 
+import com.conexa.saude.model.Agendamento;
 import com.conexa.saude.model.Medico;
+import com.conexa.saude.model.Paciente;
+import com.conexa.saude.repository.PacienteRepository;
+import com.conexa.saude.service.AgendamentoService;
 import com.conexa.saude.service.MedicoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Controller
 @RequestMapping("/api/v1")
 public class MedicoController {
 
-    public final MedicoService service;
+    public final MedicoService medicoService;
+    public final AgendamentoService agendamentoService;
+    public final PacienteRepository pacienteRepository;
 
-    public MedicoController(MedicoService service) {
-        this.service = service;
+
+    public MedicoController(MedicoService service, AgendamentoService agendamentoService, PacienteRepository pacienteRepository) {
+        this.medicoService = service;
+        this.agendamentoService = agendamentoService;
+        this.pacienteRepository = pacienteRepository;
     }
 
     @PostMapping("signup")
     public ResponseEntity<Object> cadastrarMedico(@RequestBody Medico medico) {
-        return service.cadastrar(medico);
+        return medicoService.cadastrar(medico);
     }
-
     @PostMapping("login")
     public ResponseEntity<Object> login(@RequestBody Map<String, String> loginData) {
-        return service.login(loginData);
+        return medicoService.login(loginData);
+    }
+    @PostMapping("agendamento")
+    public ResponseEntity<Object> criarAgendamento(@RequestHeader("Authorization") String token, @RequestBody Map<String, Object> agendamentoData) {
+        return agendamentoService.criarAgendamento(token, agendamentoData);
     }
 }
