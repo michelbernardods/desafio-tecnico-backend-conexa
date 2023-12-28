009dbf543b68a2bb3005561739cc78888487ecb6
Michel Bernardo <michel_13s@outlook.com>
2023-12-28 00:37:29 -0300

    perf: adding security in main spring application

--- src/main/java/com/conexa/saude/SaudeConexaApplication.java
+++ src/main/java/com/conexa/saude/SaudeConexaApplication.java
@@ -2,13 +2,14 @@ package com.conexa.saude;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SaudeConexaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SaudeConexaApplication.class, args);
 		System.out.println("OK");
 	}
-
 }
